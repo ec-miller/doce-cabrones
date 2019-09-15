c1de19337a2c7beb633a2ae53463902aea25bb2a
ec-miller <ericmiller@umn.edu>
2019-09-15 09:25:46 -0500

    Updates

---
+++ precache-manifest.66ecb585b00fc4773451ae0052a853bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0f11168e1c1e7c696196db748e2ece2",
+    "url": "/doce-cabrones/index.html"
+  },
+  {
+    "revision": "751b32b55ca7b6d83994",
+    "url": "/doce-cabrones/static/css/main.17b88157.chunk.css"
+  },
+  {
+    "revision": "70ca0c1dfe8b1bae1cb9",
+    "url": "/doce-cabrones/static/js/2.6147f74a.chunk.js"
+  },
+  {
+    "revision": "751b32b55ca7b6d83994",
+    "url": "/doce-cabrones/static/js/main.29148f7d.chunk.js"
+  },
+  {
+    "revision": "80efe782b617228602dd",
+    "url": "/doce-cabrones/static/js/runtime~main.2195e946.js"
+  },
+  {
+    "revision": "02ac192210db9789be5ba8020b15aae7",
+    "url": "/doce-cabrones/static/media/damon.02ac1922.png"
+  }
+]);
